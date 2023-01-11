7ae1f5dbe433760619de58414ff03bf6a4def89b
kattbcc17 <kattbcc17@byui.edu>
2023-01-10 19:22:32 -0700

    My first C# Fundamentals

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("This is in C#");
+
+    
+        Console.WriteLine("What's your favorite animal? ");
+        string animal = Console.ReadLine();
+        Console.WriteLine(animal);
+
+        
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");
+
+       
+
     }
+    
 }

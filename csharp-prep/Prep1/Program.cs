9a643f9731e2783c191d983ab0ea048796d6a010
kattbcc17 <kattbcc17@byui.edu>
2023-01-10 19:31:44 -0700

    Put spaces in between:)

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.WriteLine("What is your first name? ");
         string name = Console.ReadLine();
+
         Console.WriteLine("What is your last name? ");
         string lastname = Console.ReadLine();
+        
         Console.WriteLine($"Your name is {lastname}, {name} {lastname}. ");       
 
     }

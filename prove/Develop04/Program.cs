c5ee453427eb8078f4f7390a3bf4583d9825f417
kattbcc17 <kattbcc17@byui.edu>
2023-02-25 20:59:00 -0500

    working on assignments

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+       // main -- loops through menu and calls method based on choice
+       Console.WriteLine("Menu Options:" +
+        "\n1. Start breathing activity" +
+        "\n2. Start reflecting activity" +
+        "\n3. Start listing activity" +
+        "\n4. Quit" +
+        "\nSelect a choice from the menu:");
+
+        String s1 = null;
+
+        s1 = Console.ReadLine();
+
+        switch(s1){
+            case "1":
+                Console.WriteLine("You have selected breathing activity");
+                break;
+            case "2":
+                Console.WriteLine("You have selected reflecting activity");
+                break;
+            case "3":
+                Console.WriteLine("You have selected listing activity");
+                break;
+            case "4":
+                Console.WriteLine("Option Quit");
+                break;
+            default:
+                Console.WriteLine("You have selected an invalid option. Please try another option [1-4]");
+                break;
+        }
+
+        Console.ReadKey();
     }
 }
